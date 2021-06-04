0cbb36a875738ac086616c65991c8ab3ab09cfb6
Димас <52570598+GektoKot@users.noreply.github.com>
2021-06-04 13:33:37 +0200

    next commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 
 
-let firstCard = Math.floor(Math.random() * (11 - 2 + 1) + 2)
-let secondCard = Math.floor(Math.random() * (11 - 2 + 1) + 2)
+let firstCard 
+let secondCard
+let isBJ = false
+let isAlive = true
+let message = ""
+
+function startGame() {
+    firstCard = newCard()
+    secondCard = newCard()
+}
+
+function newCard() {
+    return Math.floor(Math.random() * (11 - 2 + 1) + 2)
+}
 
 let sum = firstCard + secondCard
 
-console.log(sum);
+
+if (sum <= 20) {
+    message = "Another card?"
+} else if (sum === 21) {
+    message = "YeeY! BJ and Horse"
+    isBJ = true
+} else {
+    message = "Perebor!"
+    isAlive = false
+}
+
+console.log(message);
